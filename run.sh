0b5dd1f13c3f9e4402367a1f63a919ea7380f7a4
ZeYanLin <linzeyan@users.noreply.github.com>
2018-10-25 11:26:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,11 +12,11 @@ username='service@gmail.com'           # 用戶名，請自行修改
 password='123qwe'                      # 密碼，請自行修改
 from_email_address='service@gmail.com' # 寄件者Email，請自行修改
 to_email_address="$1"                  # 收件者Email，傳入的第一個參數
-message_subject_utf8="$2"              # 郵件標題，傳入的第一個參數
-message_body_utf8="$3"                 # 郵件內容，傳入的第一個參數
-FILE=/data/file.txt
+message_subject="$2"              # 郵件標題，傳入的第一個參數
+message_body="$3"                 # 郵件內容，傳入的第一個參數
+FILE="/data/file.txt"
 
 # 發送郵件
-sendEmail='/usr/local/sendEmail/sendEmail'
+sendEmail='/usr/bin/sendemail'
 set -x
-$sendEmail -s "$SMTP_server" -xu "$username" -xp "$password" -f "$from_email_address" -t "$to_email_address" -u "$message_subject" -m "$message_body" -o message-content-type=text -o message-charset=gb2312 -o message-file=$FILE tls=yes
+$sendEmail -s "$SMTP_server" -xu "$username" -xp "$password" -f "$from_email_address" -t "$to_email_address" -u "$message_subject" -m "$message_body" -o message-content-type=text -a $FILE -o tls=yes
